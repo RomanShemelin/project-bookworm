b1df89bb2e7fba3a7fa355688e8f2d5e6757f4b5
Anna <tima2396@yandex.ru>
2022-11-09 19:15:59 +0300

    ViewCommentForms

--- views/BookCard.jsx
+++ views/BookCard.jsx
@@ -10,6 +10,7 @@ module.exports = function BookCard({ user, book }) {
         {/* <p class="card-text">{newcomment.comment}</p> */}
         {user && (user.id === book.userId) && (<button type="button" className="btn btn-danger">Удалить</button>)}
         {user && (user.id === book.userId) && (<a href={`/editbook/${book.id}`} type="button" className="btn btn-warning">Изменить</a>)}
+        <a href={`/comment/${book.id}`}type='button' className='btn btn-warning'>Комментарии</a>
       </div>
     </div>
   )
---
+++ views/Comment.jsx
@@ -0,0 +1,28 @@
+const React = require('react');
+
+const CommentCard = require('../views/CommentCard.jsx');
+const Layout = require('../views/Layout');
+const Header = require('../views/Header')
+
+
+const Commentar = ({ comments, user, book }) => {
+   return (
+      <Layout>
+         <Header user={user} />
+         <h5>Добавить комментарий</h5>
+         <form method='post' action={`/comment/${book.id}`} className='commentForm'>
+            <input className='commentInput' name='commentText' type='text'></input>
+            <button type='submit' className='commentButton'>Отправить</button>
+         </form>
+         
+         <section className='commentlist'>
+            
+            {comments.map((comment) => <CommentCard key={comment.id} comment={comment} />)}
+   
+         </section>
+            <script src='/js/comment.js'></script>
+      </Layout>
+   )
+}
+
+module.exports = Commentar
